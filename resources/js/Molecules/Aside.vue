1f89a6f497e160df347c3b520ca35920840c8b0d
kanibalkorps <birisicmartin02@gmail.com>
2023-10-06 12:23:29 +0200

    clean up

--- resources/js/Molecules/Aside.vue
+++ resources/js/Molecules/Aside.vue
@@ -1,6 +1,6 @@
 <template>
 <aside id="logo-sidebar" :class="{ 'translate-x-0': isSidebarOpen }"
-       class="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 lg:translate-x-0 dark:bg-gray-800 dark:border-gray-700"
+       class="fixed top-0 left-0 z-30 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 lg:translate-x-0 dark:bg-gray-800 dark:border-gray-700"
        aria-label="Sidebar">
 <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
     <ul class="space-y-2 font-medium">
