33f4e8ccdce92221e9f8ed09df3d08228819be2e
kanibalkorps <birisicmartin02@gmail.com>
2023-10-26 14:57:37 +0200

    Finished image upload modal confirmation. Working on adding new field from the interface, added AddNewField and Autocomplete components on Fields.vue page

---
+++ resources/js/Molecules/AddNewField.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="w-5/12 add-category flex justify-between col border mb-3 me-4 p-3 rounded-xl position-relative cursor-pointer"
+         :id="'category_'+id"
+         v-if="!showAddNew"
+         @click="showAddNew = true">
+        <div class="add-category-text">
+            <label class="text-gray-400 cursor-pointer">Add new field</label>
+        </div>
+        <div class="add-category-icon">
+            <IconPlus class="text-gray-400"/>
+        </div>
+    </div>
+    <Autocomplete v-else/>
+
+</template>
+
+<script>
+import {IconPlus} from "@tabler/icons-vue";
+import Autocomplete from "@/Molecules/Autocomplete.vue";
+
+export default {
+    name: "AddNewField",
+    components: {
+        IconPlus,
+        Autocomplete
+    },
+    props: {
+        id: Number
+    },
+    data() {
+        return {
+            showAddNew: false
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
