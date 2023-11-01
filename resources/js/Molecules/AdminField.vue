0b2241426c09fc7a6cf20db3b9454fb4274eb691
kanibalkorps <birisicmartin02@gmail.com>
2023-11-01 20:05:05 +0100

    Enabled modal popping up on field settings icon click. Now just to print settings, collect and save them.

--- resources/js/Molecules/AdminField.vue
+++ resources/js/Molecules/AdminField.vue
@@ -6,28 +6,35 @@
                    :for="'field_check_'+field_id" class="hover:cursor-grab active:cursor-grabbing select-none">
                 {{ title }}
             </label>
-            <IconAdjustments :id="'icon_'+field_id" :data-field-name="title"
-                             class="cursor-pointer font-light select-none"/>
-
-            <div :id="'field_check_'+field_id" class="hidden checkboxes">
-                <label :for="'active_'+field_id" class="flex items-center mb-1">
-                    <input :id="'active_'+field_id" :value="field_id" checked="checked" name="fields[]"
-                           type="checkbox"/>
-                    <span class="ml-2">Active</span>
-                </label>
-                <label :for="'required_'+field_id" class="flex items-center">
-                    <input :id="'required_'+field_id" :checked="is_required ? 'checked' : '' " :value="field_id"
-                           name="requiredFields[]"
-                           type="checkbox"/>
-                    <span class="ml-2">Required</span>
-                </label>
+            <div class="mb-field-settings-icon">
+                <IconAdjustments :id="'icon_'+field_id" :data-field-name="title"
+                                 class="cursor-pointer font-light select-none" @click="openModal = true"/>
             </div>
+            <Modal v-if="openModal" @close="openModal=false">
+                <template v-slot:modalTitle>Field "{{ title }}" configurations</template>
+                <template v-slot:modalContent>Content</template>
+                <!--                <template v-slot:modalFooter>Footer</template>-->
+            </Modal>
+            <!--            <div :id="'field_check_'+field_id" class="hidden checkboxes">-->
+            <!--                <label :for="'active_'+field_id" class="flex items-center mb-1">-->
+            <!--                    <input :id="'active_'+field_id" :value="field_id" checked="checked" name="fields[]"-->
+            <!--                           type="checkbox"/>-->
+            <!--                    <span class="ml-2">Active</span>-->
+            <!--                </label>-->
+            <!--                <label :for="'required_'+field_id" class="flex items-center">-->
+            <!--                    <input :id="'required_'+field_id" :checked="is_required ? 'checked' : '' " :value="field_id"-->
+            <!--                           name="requiredFields[]"-->
+            <!--                           type="checkbox"/>-->
+            <!--                    <span class="ml-2">Required</span>-->
+            <!--                </label>-->
+            <!--            </div>-->
         </div>
     </div>
 </template>
 
 <script>
 import {IconAdjustments} from '@tabler/icons-vue';
+import Modal from "@/Molecules/Modal.vue";
 
 export default {
     name: "AdminField",
@@ -37,8 +44,14 @@ export default {
         is_required: Boolean,
     },
     components: {
+        Modal,
         IconAdjustments
     },
+    data() {
+        return {
+            openModal: false
+        }
+    }
 }
 </script>
 
