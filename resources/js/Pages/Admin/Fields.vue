91cf79abf0f611f54cd2e66bbe4de3a46ed8f57c
kanibalkorps <birisicmartin02@gmail.com>
2023-11-04 11:54:57 +0100

    Fiex Admin Fields page

--- resources/js/Pages/Admin/Fields.vue
+++ resources/js/Pages/Admin/Fields.vue
@@ -3,35 +3,7 @@
     <AuthenticatedLayout>
         <div class="grid grid-cols-1 mt-24">
             <div v-for="(category, key) in categories" :key="key" class="col">
-                <div class="p-6 px-8 mb-20 border rounded-3xl bg-white">
-                    <form>
-                        <div class="flex justify-between">
-                            <h3 class="mb-10 text-lg font-bold">{{ category.category_name }}</h3>
-                            <input class="mb-10 " name="submit-btn" type="submit"
-                                   value="Submit"/>
-                        </div>
-                        <draggable :component-data="{
-                                          tag: 'AdminField',
-                                          type: 'transition-group',
-                                          name: !drag ? 'slide' : null,
-                                          pull: 'clone',
-                                        }"
-                                   :group="'fields_'+category.field_category_id"
-                                   :list="category.fields"
-                                   class="fields-container flex flex-wrap"
-                                   item-key="field_id"
-                                   v-bind="dragOptions"
-                                   @end="drag=false"
-                                   @start="drag=true">
-                            <template #item="{ element }" class="">
-                                <AdminField :field_id="element.field_id" :is_required="element.required"
-                                            :title="element.title ?? element.field_name"/>
-                            </template>
-                        </draggable>
-                        <hr class="mb-3"/>
-                        <AddNewField :catId="category.field_category_id" :order="category.fields.length + 1"/>
-                    </form>
-                </div>
+                <AdminFieldCategory :category="category"/>
             </div>
         </div>
     </AuthenticatedLayout>
@@ -40,40 +12,18 @@
 <script>
 import {Head} from '@inertiajs/vue3';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import AdminField from '@/Molecules/AdminField.vue';
-import {IconPlus} from '@tabler/icons-vue';
-import draggable from 'vuedraggable';
-import AddNewField from '@/Molecules/AddNewField.vue';
 import {provide} from 'vue';
-// import { toRef, provide } from 'vue'
+import AdminFieldCategory from "@/Organisms/AdminFieldCategory.vue";
 
 export default {
     name: 'Fields',
     components: {
-        AdminField,
+        AdminFieldCategory,
         AuthenticatedLayout,
         Head,
-        IconPlus,
-        draggable,
-        AddNewField,
     },
     props: {
         categories: Array,
-    },
-    data() {
-        return {
-            drag: false,
-            showAddNew: false,
-        };
-    },
-    computed: {
-        dragOptions() {
-            return {
-                animation: 400,
-                disabled: false,
-                ghostClass: 'ghost',
-            };
-        },
     },
     provide() {
         return {
@@ -107,18 +57,4 @@ export default {
 };
 </script>
 
-<style scoped>
-.slide-move {
-    transition: transform 0.5s;
-}
-
-.no-move {
-    transition: transform 0s;
-}
-
-.ghost {
-    opacity: 0.5;
-    background: #efefef;
-}
-</style>
 
