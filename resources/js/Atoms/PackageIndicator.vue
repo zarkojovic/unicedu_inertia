ec5f6b1c9e1d47feaa5faa0e8e9fbeb191ca43ef
zarkojovic <113613621+zarkojovic@users.noreply.github.com>
2023-12-14 11:49:49 +0100

    pacakge indicator

--- resources/js/Atoms/PackageIndicator.vue
+++ resources/js/Atoms/PackageIndicator.vue
@@ -7,7 +7,7 @@
 </template>
 
 <script setup>
-import {computed, onMounted, ref} from 'vue';
+import {computed} from 'vue';
 import {usePage} from '@inertiajs/vue3';
 
 const page = usePage();
@@ -22,43 +22,6 @@ const props = defineProps({
 const activePackage = computed(() => {
     return page.props.packages.filter(el => el.package_id === props.packageId)[0];
 });
-
-onMounted(() => {
-    console.log(activePackage.value);
-});
-
-const packageTitle = ref(null);
-
-const packageClass = computed(() => {
-    const classes = {
-        'package-indicator': true,
-        'bronze-package': props.packageId === 1,
-        'silver-package': props.packageId === 2,
-        'gold-package': props.packageId === 3,
-        'platinum-package': props.packageId === 4,
-    };
-
-    switch (props.packageId) {
-        case 1:
-            packageTitle.value = 'Bronze';
-            break;
-        case 2:
-            packageTitle.value = 'Silver';
-            break;
-        case 3:
-            packageTitle.value = 'Gold';
-            break;
-        case 4:
-            packageTitle.value = 'Platinum';
-            break;
-        default:
-            packageTitle.value = 'Bronze';
-    }
-
-    return classes;
-});
-
-
 </script>
 
 <style scoped>
@@ -70,26 +33,5 @@ const packageClass = computed(() => {
     text-transform: uppercase;
     color: #fff;
     letter-spacing: 0.5px;
-}
-
-.gold-package {
-    background: #f5df6c;
-    background: linear-gradient(to right, #f5df6c, #e0c340);
-}
-
-.silver-package {
-    background: #757f9a; /* fallback for old browsers */
-    background: -webkit-linear-gradient(to right, #d7dde8, #757f9a); /* Chrome 10-25, Safari 5.1-6 */
-    background: linear-gradient(to right, #d7dde8, #757f9a); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-}
-
-.platinum-package {
-    background: #c2cde4;
-    background: linear-gradient(to right, #c2cde4, #d3b1df);
-}
-
-.bronze-package {
-    background: #e6b278;
-    background: linear-gradient(to right, #e6b278, #c28342);
 }
 </style>
